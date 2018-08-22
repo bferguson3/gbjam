c5c51f4332df9bd795008cd0084fbb2e80856cfc
B. F <bferguson@twitter.com>
2018-08-22 10:22:45 -0700

    awesome sprite routine

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm gbjamtiles.asm
-./gb-convert -i -tiles tiles.png >> gbjamtiles.asm
+#rm gbjamtiles8000.asm
+#rm gbjamtiles9000.asm
+#./gb-convert -i -tiles gbjamtiles8000.png >> gbjamtiles8000.asm
+#./gb-convert -i -tiles gbjamtiles9000.png >> gbjamtiles9000.asm
 rgbasm -o game.o game.z80
 rgblink -o game.gb -p 0xff game.o
 rgbfix -p 0xff -v game.gb
